2c5c402ce2eefd03b2df3e3178ddb03cc722b133
onlybecool <only.becool5@gmail.com>
2014-03-18 16:40:24 +0100

    Tomis netw_v1

---
+++ Minesweeper/src/minesweeper/clientsList.java
@@ -0,0 +1,37 @@
+package minesweeper;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class clientsList implements Serializable{
+
+	private static final long serialVersionUID = 2296359732912201133L;
+	private ArrayList<clients> clientList;
+	
+	clientsList() {
+		clientList = new ArrayList<clients>();
+	};
+	
+	void addClient(clients	clientInstance){
+		clientList.add(clientInstance);
+	};
+	
+	void addClient(int n, String s){
+		clients client=  new clients(n, s);
+    	addClient(client);
+	};
+	
+	void printClientList(){
+		System.out.println("");
+		for(int i=0; i< clientList.size();i++)
+			System.out.println(clientList.get(i).getID() + ". " + clientList.get(i).getclientName());
+	};
+	
+	protected clientsList(clientsList original){
+		clientList = new ArrayList<clients>(original.clientList);
+	}
+	
+	public clientsList copy(){
+		return new clientsList(this);
+	}
+}
