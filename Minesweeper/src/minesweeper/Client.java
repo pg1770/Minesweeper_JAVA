a612c1e5a49bf986c5443c0359c33f6603ca34a1
onlybecool <only.becool5@gmail.com>
2014-04-18 02:14:38 +0200

    elvileg oke

--- Minesweeper/src/minesweeper/Client.java
+++ Minesweeper/src/minesweeper/Client.java
@@ -1,106 +1,120 @@
 package minesweeper;
 
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Client extends Network{
+public class Client extends Control{
 	 
 	 private Socket 			clientSocket = null; 
 	 private ObjectOutputStream out = null;
 	 private ObjectInputStream 	in = null;
-	 private final int 			port = 9898; 
-	 private String 			clientName;
-	 private clientsList		clientList;
 	 private Object 			fromServerObject;
-	 
-	 Client(Control c){
-		 super(c);
+	 protected Control 			ctrl;
+		
+	 protected Client(Control c){
+			ctrl = c;
 	 }
 	 	  
 	 private class ReceiverThread implements Runnable{
 		 public void run(){
-			 System.out.println("Login to the server!");
 			 try {
-				 send(clientName);
 				 while(((fromServerObject = in.readObject()) != null ) && clientSocket.isConnected()){
 					 
-					 if (fromServerObject.getClass() == clientsList.class){
-						 clientList = (clientsList) fromServerObject;
-						 //ctrl.clientReceived(clientList);
-						 clientList.printClientList();
+					 if(fromServerObject instanceof Scores){
+						 Scores scoreTable = (Scores) fromServerObject;
+						 ctrl.receiveScores(scoreTable);
 					 }
 					 
-					 if(fromServerObject.getClass() == String.class){
-						 String fromServerString = (String) fromServerObject;
-						 System.out.println(fromServerString); 
+					 if(fromServerObject instanceof TimeStamp){
+						 TimeStamp timeStamp = (TimeStamp) fromServerObject;
+						 ctrl.receiveGameTime(timeStamp);
 					 }
+					 
+					 if(fromServerObject instanceof GameInfo){
+						 GameInfo gameInfo = (GameInfo) fromServerObject;
+						 ctrl.receiveGameInfo(gameInfo);
+					 }
+					 
+					 if(fromServerObject instanceof PlayersList){
+						 PlayersList players = (PlayersList) fromServerObject;
+						 ctrl.playersListPrint(players);
+					 }
+ 
 				 }
 			 } catch (ClassNotFoundException classNot) {
-			       System.err.println("data received in unknown format");
+				 ctrl.clientError("data received in unknown format");  
 			 }	catch(Exception e){
-				 System.err.println("Server disconnected!");
-				 System.err.println(e.getMessage());
+				 ctrl.clientError("Server disconnected!");
 			 }	finally {
 				 disconnect();
 			 }
 		}
 	 }
 	 
-	 @Override
-	 void connect(String ip, String name){
-		 clientName=name;
+	 void connect(){
 		 disconnect();
 		 try{
-			 clientSocket = new Socket(ip, port);
+			 clientSocket = new Socket(NetworkDefines.ip, NetworkDefines.port);
 			 out = new ObjectOutputStream(clientSocket.getOutputStream());
 	         in = new ObjectInputStream(clientSocket.getInputStream());
 	         out.flush();
-	         clientList = new clientsList();
-	         Runnable runnable = new ReceiverThread();
-	    	 Thread thread = new Thread(runnable);
-	    	 thread.start();
+	         Thread thread = new Thread(new ReceiverThread());
+	         thread.start();
 		 }	catch(UnknownHostException e) {
-	         System.err.println("Don't know about host");
+			 ctrl.clientError("Don't know about host");
 		 }  catch(Exception e){
-			 System.err.println("Cannot connect to server!");
-			 System.err.println(e.getMessage());
+			 ctrl.clientError("Cannot connect to server!");
 		 }
 	 }
-
-	 @Override
-	 void send(Object p) {
-		 if (out == null) return;
-	     try {
-	    	 out.writeObject(p);
-	         out.flush();
-	     }	catch (Exception e) {
-	         System.err.println("Send error.");
-	         System.err.println(e.getMessage());
-	     }
-	 }
 	 
+	 void setName(String clientName){
+		 send(clientName);
+	 }
+
 	 void send(String str){
 		 if (out == null) return;
 	     try {
 	        out.writeObject(str);
 	        out.flush();
+	        out.reset();
 	     }	catch (IOException e) {
-	    	 System.err.println("Send error.");
-	    	 System.err.println(e.getMessage());
+	    	 ctrl.clientError("Send error!");
 	     }
 	 }
 	 
+	 void send(int tableSize){
+		 if (out == null) return;
+	     try {
+	        out.writeObject(tableSize);
+	        out.flush();
+	        out.reset();
+	     }	catch (IOException e) {
+	    	 ctrl.clientError("Send error!");
+	     }
+	 }
+	 
+	 void send(ClickEvent	clickEvent){
+		 if (out == null) return;
+	     try {
+	        out.writeObject(clickEvent);
+	        out.flush();
+	        out.reset();
+	     }	catch (IOException e) {
+	    	 ctrl.clientError("Send error!");
+	     }
+	 }
+	  
 	 void disconnect() {
-	        try {
-	        	if (out != null) out.close();
-	        	if (in != null) in.close();
-	        	if (clientSocket != null) clientSocket.close();
-	        } catch (IOException e) {
-	        	System.err.println("Error while closing connection!");
-	        	System.err.println(e.getMessage());
-	        }
-	    }
+		 try {
+			 if (out != null) out.close();
+			 if (in != null) in.close();
+			 if (clientSocket != null) clientSocket.close();
+		 } catch (IOException e) {
+			 ctrl.clientError("Error while closing connection!");
+	     }
+	 }
 }
