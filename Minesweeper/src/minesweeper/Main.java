ef14fa3d1480afa60ceb253e4f715ca6c9b331c7
pg1770 <pecsigergely@gmail.com>
2014-04-22 01:35:42 +0200

    asfasf

--- Minesweeper/src/minesweeper/Main.java
+++ Minesweeper/src/minesweeper/Main.java
@@ -11,19 +11,12 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Control x = new Control(10,10,20);
+		Control x = new Control();
 	  GUI k = new GUI(x);
     x.setGUI(k);
-    x.GameStart(2);
+//    x.GameStart(2);
     x.timer.schedule(new TimeStamp(), 0, 1000 );
     
-    if ((args.length > 1) ){
-    	if(args[0].equals("s")){
-    		x.startServer();
-    	}	else if(args[0].equals("c")){
-    		x.startClient();	
-    	}
-    }
     
 	}
 
